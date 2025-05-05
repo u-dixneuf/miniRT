1ba6a217940d27b4f0733c202487072bb18562a2
Mohamed Aymane Zary <mzary@e1r10p11.1337.ma>
2025-05-05 15:26:37 +0100

    [optimus] - l3gezzz

--- src/main.c
+++ src/main.c
@@ -7,16 +7,13 @@ int	main(int ac, char **av)
 
 	if (ac != 2)
 		return (mrt_error(ARGS_NUM), R_INVALID);
-	if (mrt_init(&mrt))
-		return (R_MALLOC);
+	mrt_init(&mrt);
 	ret = mrt_parser(av[1], &mrt);
 	if (ret)
 		return (mrt_free_all(&mrt), ret);
-	
-	printf("success");
-	// ret = mrt_initmlx(&mrt.mlx);
-	// if (ret)
-	// 	return (mrt_free_all(&mrt), ret);
+	ret = mrt_initmlx(&mrt.mlx);
+	if (ret)
+		return (mrt_free_all(&mrt), ret);
 	// ret = mrt_viewer(&mrt);
 	// if (ret)
 	// 	return (mrt_free_all(&mrt), ret);
