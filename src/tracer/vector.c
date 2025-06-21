5c8c7deafed23438359a5316ba77120fc17cab88
Mohamed Aymane Zary <mzary@e1r9p9.1337.ma>
2025-06-21 04:41:23 +0100

    default message

--- src/tracer/vector.c
+++ src/tracer/vector.c
@@ -21,6 +21,7 @@ void	get_first_vector(t_camera camera, t_ray *ray)
 		i += 1;
 	}
 	normalize_vector(ray->vector);
+	printf("ray[%d][%d], vector [%lf][%lf][%lf]\n", ray->h, ray->w, ray->vector[0], ray->vector[1], ray->vector[2]);
 }
 
 // void		get_second_vector(t_light light, ?);
